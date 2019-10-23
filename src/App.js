b09b1353329506d94512ab15159b168740632cd1
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-10-23 13:03:02 +0200

    updating shop page, preview component, collection component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./page/homepage.component";
+import HomePage from "./page/homepage/homepage.component";
+import ShopPage from './page/shop/shop.component';
 import {Switch, Route} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>tst</div>
-)
 
 function App() {
   return (
     <>
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   </>
   );
