dced96bfead96666cda71bd0513422c5d988e8fc
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-10-23 10:24:46 +0200

    menu-items withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./page/homepage.component";
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>tst</div>
+)
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </Switch>
+  </>
+  );
 }
 
 export default App;
