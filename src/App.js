e6bfda9b9ebde8ecf994d9b5bc30bcc6fd1a0471
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-10-28 16:04:29 +0100

    redux tol

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import HomePage from "./page/homepage/homepage.component";
 import ShopPage from './page/shop/shop.component';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUp from './page/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
-import {setCurrentUser}
- from'./redux/user/user.actions'
+import {setCurrentUser} from'./redux/user/user.actions'
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -36,20 +36,34 @@ class App extends React.Component {
 
   render() {
     return (
-        <>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
-        </Switch>
-        </>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route
+                exact
+                path='/signin'
+                render={() =>
+                    this.props.currentUser ? (
+                        <Redirect to='/' />
+                        ) : (
+                            <SignInAndSignUp />
+                        )
+                }
+            />
+          </Switch>
+        </div>
     );
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+import {createStore, compose} from 'redux';
+// import {createStore, applyMiddleware, compose} from 'redux';
+// import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+// const middlewares = [logger];
+//
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer());
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export default store;
