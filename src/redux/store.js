f822e80e188fa3b71896b1ddceda6002edc3d6e7
Peter Popluhar <popluhar.peter@gmail.com>
2019-11-03 16:14:17 +0100

    redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import {createStore, compose} from 'redux';
 // import {createStore, applyMiddleware, compose} from 'redux';
 // import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 
 // const middlewares = [logger];
 //
 // const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancer());
+const persistor = persistStore(store);
 
-export default store;
+export {store, persistor};
