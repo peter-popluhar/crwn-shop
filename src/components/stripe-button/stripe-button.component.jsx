55ef31af5c5501d55bc1bacbb5d3b7e8136702f9
Peter Popluhar <popluhar.peter@gmail.com>
2019-11-04 21:44:19 +0100

    stripe payment integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_MXdyvDMBiyDO5I5NlksXnPVv00Z7t7n1KS';
+    const onToken = token => {
+        console.log(token);
+        alert('payment succesfull')
+    }
+
+
+    return (
+        <StripeCheckout
+            label='pay now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
