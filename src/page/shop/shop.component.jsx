304dce4fc687b3b648ed5dbe7e70d7a5707f4de1
Peter Popluhar <popluhar.peter@gmail.com>
2019-11-24 13:13:20 +0100

    asynchronous redux - thunk

--- src/page/shop/shop.component.jsx
+++ src/page/shop/shop.component.jsx
@@ -3,52 +3,50 @@ import CollectionsOverview from './../../components/collections-overview/collect
 import {Route} from 'react-router-dom';
 import CollectionPage from '../collection/collection.component.jsx';
 import {firestore, convertCollectionsSnapshotsToMap} from '../../firebase/firebase.utils'
-import {updateCollections} from './../../redux/shop/shop.actions'
+import {fetchCollectionsStartAsync} from './../../redux/shop/shop.actions'
 import {connect} from 'react-redux'
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import {createStructuredSelector} from 'reselect';
+import {selectIsCollectionFetching, selectIsCollectionLoaded} from './../../redux/shop/shop.selectors';
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    }
-
-    unsubscribeFromSnapshot = null;
-
     componentDidMount() {
-        const {updateCollections} = this.props;
-        const collectionRef = firestore.collection('collections');
+        const {fetchCollectionsStartAsync} = this.props;
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotsToMap(snapshot)
-            updateCollections(collectionsMap)
-            this.setState({loading: false})
-        })
-
-
+        fetchCollectionsStartAsync();
     }
 
     render() {
-        const {match} = this.props;
-        const {loading} = this.state;
-        console.log(loading)
+        const {match, isCollectionFetching, isCollectionLoaded} = this.props;
+
+        console.log(isCollectionFetching)
+
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
-                <Route path={`${match.path}/:collectionId`} render={props => (<CollectionPageWithSpinner isLoading={loading} {...props} />)}/>
+                <Route exact path={`${match.path}`}
+                       render={(props) => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props}
+                       />} />
+                <Route path={`${match.path}/:collectionId`}
+                       render={props => (<CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props} />)}
+                />
             </div>
         )
     }
 
 }
 
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionLoaded: selectIsCollectionLoaded
+})
+
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap =>
-        dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
 
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,18 @@
-import {createStore, compose} from 'redux';
-// import {createStore, applyMiddleware, compose} from 'redux';
-// import logger from 'redux-logger';
+import {createStore, applyMiddleware, compose} from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
+import thunk from 'redux-thunk'
 
-// const middlewares = [logger];
-//
-// const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const middlewares = [thunk];
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancer());
+//const store = createStore(rootReducer, composeEnhancer());
 const persistor = persistStore(store);
 
 export {store, persistor};
