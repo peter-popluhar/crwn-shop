58c2207ad98e0dcbe2f5a547d302d49797dbd149
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-12-17 17:04:05 +0100

    react profiler

--- client/src/page/homepage/homepage.component.jsx
+++ client/src/page/homepage/homepage.component.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {Profiler} from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
-      <Directory />
+        <Profiler id="directory" onRender={(id, phase, actualDuration) =>
+        console.log({id, phase, actualDuration})}>
+            <Directory />
+        </Profiler>
     </div>
   </div>
 );
